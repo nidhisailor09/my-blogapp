cc2833e444eaff175d007bcaa051fd198ca3937e
Nidhi Sailor <nidhi99.sailor@gmail.com>
2024-01-15 16:01:01 -0500

    Third commit

--- src/server.js
+++ src/server.js
@@ -1,42 +1,72 @@
 import express from "express";
+import { db, connectToDb } from "./db.js";
+// let articlesInfo=[{
+//     name: 'learn-react',
+//     upvotes: 0,
+//     comments: [], //adding an empty array
 
-let articlesInfo=[{
-    name: 'learn-react',
-    upvotes: 0,
-    comments: [], //adding an empty array
-
-},{
-    name: 'learn-node',
-    upvotes: 0,
-    comments: [],
-},{
-    name: 'mongodb',
-    upvotes: 0,
-    comments: [],
-}]
+// },{
+//     name: 'learn-node',
+//     upvotes: 0,
+//     comments: [],
+// },{
+//     name: 'mongodb',
+//     upvotes: 0,
+//     comments: [],
+// }]
 
 const app = express();
 app.use(express.json());
 
+app.get('/api/articles/:name', async (req,res)=>{
+   const { name } = req.params;
+    
+   
+    const article = await db.collection('articles').findOne({ name });
+    if(article){
+        res.json(article); // res.json bcoz we are sending json data
+    }
+    else{
+        res.sendStatus(404);
+    }
+});
+
 //creating an end point
-app.put('/api/articles/:name/upvote',(req,res)=>{
+app.put('/api/articles/:name/upvote', async(req,res)=>{
     const { name } = req.params;
-    const article = articlesInfo.find(a => a.name === name);
+    // const article = articlesInfo.find(a => a.name === name);
+ /**************** */
+
+    
+    await db.collection('articles').updateOne({ name }, {
+         $inc: { upvotes: 1},
+    
+    });
+    const article = await db.collection('articles').findOne({ name });
+/**************** */
     if (article){
-        article.upvotes +=1;
+        // article.upvotes +=1;
         res.send(`The ${name} article now has ${article.upvotes} upvote!!!`);
     }else{
         res.send('That article doesn\'t exist');
     }
 });
 
-app.post('/api/articles/:name/comments',(req,res)=>{
+app.post('/api/articles/:name/comments',async(req,res)=>{
     const { name } = req.params;
     const { postedBy, text }=req.body;
-    const article = articlesInfo.find(a => a.name === name);
+    //const article = articlesInfo.find(a => a.name === name);
+/**************** */
+
+ 
+    await db.collection('articles').updateOne({ name }, { 
+        $push: { comments: { postedBy,text} },
+    });
+    const article = db.collection('articles').findOne({ name });
+/**************** */
 
     if(article){
-        article.comments.push({postedBy, text});
+       // article.comments.push({postedBy, text});
         res.send(article.comments);
     }else{
         res.send('That article doesn\'t exist');
@@ -56,7 +86,9 @@ app.post('/api/articles/:name/comments',(req,res)=>{
 //     res.send(`Hello ${name}!!`);
 
 // });
-
-app.listen(8000,() => {
-    console.log('Server is running on port 8000');
+connectToDb(()=>{
+    console.log("Successfully connected to database!")
+    app.listen(8000,() => {
+        console.log('Server is running on port 8000');
+    });
 })
