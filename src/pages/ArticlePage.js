c6bc6900a9d64f9bc008373384246f1ef27234cd
Nidhi Sailor <nidhi99.sailor@gmail.com>
2024-01-15 16:38:17 -0500

    FrontEnd Commit

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,10 +1,22 @@
 import { useParams } from "react-router-dom";
+import NotFoundPage from './NotFoundPage';
+import articles from './article-content';
+
 const ArticlePage = () => {
     const { articleId } = useParams();
-    // const articleId = params.articleId;
-   // const { articleId } = params;
+    const article = articles.find(article => article.name === articleId);
+
+    if (!article) {
+        return <NotFoundPage />
+    }
+
     return (
-        <h1>This is the article page for the id: {articleId}! </h1>
+        <>
+        <h1>{article.title}</h1>
+        {article.content.map((paragraph, i) => (
+            <p key={i}>{paragraph}</p>
+        ))}
+        </>
     );
 }
 
